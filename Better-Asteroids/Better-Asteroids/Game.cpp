71b0a833ab17f7e50525a0f87adc94acfa528e96
julian serulnikov <julianholisu@gmail.com>
2018-10-02 09:49:45 -0300

    gitignore agregado, rotacion encaminada

--- Better-Asteroids/Better-Asteroids/Game.cpp
+++ Better-Asteroids/Better-Asteroids/Game.cpp
@@ -3,12 +3,12 @@ namespace flowspace {
 	namespace gamespace {
 		Texture2D ship;
 		player p1;
-		Vector2 colliderAllignment;
 		int shipswidth = 237;
 		int shipsheight = 291;
-		float shipscale = 0.35f;
+		float shipscale = 2.0f;
 		Rectangle sourceRec = { 0.0f, 0.0f, (float)shipswidth, (float)shipsheight};
-		Rectangle destRec = { (float)screenwidth / 2, (float)screenheight / 2, (float)shipswidth , (float)shipsheight};
+		Rectangle destRec = { (float)screenwidth / 2, (float)screenheight / 2, (float)shipswidth * shipscale , (float)shipsheight * shipscale};
+		Vector2 colliderAllignment = { (float)shipswidth, (float)shipsheight };
 
 		void initGameplay() {
 			p1.position.x = screenwidth / 2;
@@ -31,12 +31,14 @@ namespace flowspace {
 				p1.position.y += 2;
 			}
 			if (IsKeyDown(KEY_D)) {
-				p1.rotation -= 0.5f;
+				p1.rotation += 0.2f;
 			}
 			if (IsKeyDown(KEY_A)) {
-				p1.rotation += 0.5f;
+				p1.rotation -= 0.2f;
 			}
-			colliderAllignment = { p1.position.x + (237 / 2 * shipscale) ,p1.position.y - (291 / 2 * shipscale) };
+			destRec.x = p1.position.x;
+			destRec.y = p1.position.y;
+			
 		}
 		void drawGame() {
 			DrawTexturePro(ship, sourceRec,destRec ,colliderAllignment, p1.rotation ,WHITE);
